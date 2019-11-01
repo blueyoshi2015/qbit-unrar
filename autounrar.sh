cf665df48e9ec45920d780f83fb47200a0358df9
blueyoshi2015 <57151788+blueyoshi2015@users.noreply.github.com>
2019-11-01 16:13:18 -0400

    Update autounrar.sh

--- autounrar.sh
+++ autounrar.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # This calls unrar and passes the variable for the path to the file and extract path.
-/usr/bin/unrar e -r "${1}"/  "${1}"/
+/usr/bin/unrar e "${1}"/*.rar  "${1}"/
+
 
 # Sends the value for the variable to a log file for diagnostic purposes.
 /bin/echo "${1}" >> /tmp/qbit.log
